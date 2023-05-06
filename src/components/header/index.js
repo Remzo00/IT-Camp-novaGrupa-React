3d8a2efb920181f4cbf2ef9bbd4336255f21938d
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-05-06 21:10:31 +0200

    Komponente

---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Logo</h1>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
