ecd9681963951ca85501e9252d13fe7e99dd7b8e
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-05-16 19:19:01 +0200

    Komponente

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import Button from './components/button/button';
 import Footer from './components/footer';
 import Header from './components/header';
 import Main from './components/main';
 
-//JSX
 function App() {
   return (
     <div className="App">
         <Header />
         <Main />
         <Footer />
+        <Button text={'Dodaj'} color={'red'} />
+        <Button text={'Izbrisi'} color={'blue'} />
+        <Button text={'Log in'} color={'green'}/>
     </div>
   );
 }
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from '../button/button';
 
 const Main = () => {
     return(
         <div>
-            <p>neki tekst</p>
+            <Button  />
         </div>
     )
 }
