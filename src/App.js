0078c7ae88733089617455c5ddb6145156baa0f4
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-06-20 19:17:47 +0200

    add search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button } from '@mui/material';
+import axios from 'axios';
+import SearchBar from './components/search';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      const data = response.data;
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData()
+  })
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
-    <div className='container'>
-      <Button>Test</Button>
-    </div>
+    <div>
+    <SearchBar handleSearch={handleSearch} />
+
+    {filteredProducts.map((product) => (
+      <div key={product.id}>
+        <h3>{product.title}</h3>
+      </div>
+    ))}
+  </div>
   );
 }
 
