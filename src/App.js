1ac2a9504ca1f0d3d08bcff6474a7deeeef92a11
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-06-20 17:59:36 +0200

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-import Header from './pages/header';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import ParentComponent from './props/ParentComponent';
+import { Button } from '@mui/material';
 
-//npm i react-router-dom
-//npm install axios
 function App() {
-  const [data, setData] = useState([])
-
-  const fecthData = async () => {
-    try{
-      const response = await axios.get('https://fakestoreapi.com/products')
-      setData(response.data)
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    fecthData()
-  }, [])
-
-//   {
-//     id:1,
-//     title:'...',
-//     price:'...',
-//     category:'...',
-//     description:'...',
-//     image:'...'
-// }
-
-
   return (
     <div className='container'>
-      <ParentComponent />
-      {/* {data.map((product) => (
-        <div>
-          <img src={product.image} />
-        </div>
-
-      ))} */}
+      <Button>Test</Button>
     </div>
   );
 }
