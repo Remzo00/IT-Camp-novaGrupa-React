926ab091ad28acae761463fafc45857fcfa1ceb3
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-06-22 19:04:06 +0200

    React Query

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
-import SearchBar from './components/search';
+import ReactQuery from './components/query/query';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('https://fakestoreapi.com/products');
-      const data = response.data;
-      setProducts(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData()
-  })
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
-  const filteredProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
   return (
-    <div>
-    <SearchBar handleSearch={handleSearch} />
-
-    {filteredProducts.map((product) => (
-      <div key={product.id}>
-        <h3>{product.title}</h3>
-      </div>
-    ))}
-  </div>
+    <ReactQuery />
   );
 }
 
